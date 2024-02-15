f972dc17c59c3bb004482f5192900004089cd362
Shawn Song <sonykey2003@gmail.com>
2024-02-15 16:24:55 +0800

    Update instance.tf

--- AD/instance.tf
+++ AD/instance.tf
@@ -1,12 +1,45 @@
+# AWS Auth - Using SSO profile
 provider "aws" {
 
-  profile = "shawn-sedemo-admin"
+  profile = var.my-aws-profile
 }
 
+
+# Building the instances
+resource "aws_instance" "ad-lab" {
+  for_each = var.ad-lab-instances
+  ami = each.value.ami
+  instance_type = "t2.medium"
+  user_data_replace_on_change = true
+  associate_public_ip_address = true
+  key_name               = aws_key_pair.key_pair.key_name
+  get_password_data      = false
+  vpc_security_group_ids = [aws_security_group.allow-rdp.id,aws_security_group.allow-internal-all.id]
+  subnet_id = aws_subnet.adlab-subnet.id
+  tags = {
+    Name = "win2022-${each.value.role}"
+  }
+
+  connection {
+    host     = coalesce(self.public_ip, self.private_ip)
+    type     = "winrm"
+    timeout  = "5m"
+    https    = true
+    port     = 5986
+    use_ntlm = true
+    insecure = true
+    user     = "Administrator"
+    password = var.admin_pw
+  }
+  user_data = each.value.role == "DC" ? data.cloudinit_config.ad.rendered:data.cloudinit_config.client.rendered
+}
+
+
+/*
 resource "aws_instance" "dc01" {
 
   # DC specs
-  ami                    = var.amis[var.AWS_REGION]
+  ami                    = var.win-amis[var.AWS_REGION]
   user_data_replace_on_change = true
   associate_public_ip_address = true
   instance_type          = "t2.medium"
@@ -38,7 +71,7 @@ resource "aws_instance" "c01" {
   count = 1
 
   # Client specs
-  ami                    = var.amis[var.AWS_REGION]
+  ami                    = var.win-amis[var.AWS_REGION]
   user_data_replace_on_change = true
   associate_public_ip_address = true
   instance_type          = "t2.medium"
@@ -64,35 +97,39 @@ resource "aws_instance" "c01" {
 
   user_data = data.cloudinit_config.client.rendered
 }
+*/
 
 
 output "Administrator_Username" {
   value = "Administrator"
   
 }
-output "Administrator_Password" {
-  value = nonsensitive(var.admin_pw)
-  
+
+output "public_ip_info" {
+  value = [for instance in aws_instance.ad-lab: "${instance.tags.Name}:${instance.public_ip}"]
 }
 
-
-output "dc01_ip_info" {
-  value = [aws_instance.dc01.*.public_ip,aws_instance.dc01.*.private_ip]
-}
-
-output "dc01_dns_info" {
-  value = [aws_instance.dc01.*.public_dns,aws_instance.dc01.*.private_dns]
+output "public_dns_info" {
+  value = [for instance in aws_instance.ad-lab: "${instance.tags.Name}:${instance.public_dns}"]
 }
 
 output "note" {
   value = "Please give it 5~10 min before RDP-ing as the AD script is busy doing its job, go grab a coffee! :-) "
 }
-
-output "c01_ip_info" {
-  value = [aws_instance.c01.*.public_ip,aws_instance.c01.*.private_ip]
+/*
+output "Administrator_Password" {
+  value = nonsensitive(var.admin_pw)
+  
 }
 
-output "c01_dns_info" {
-  value = [aws_instance.c01.*.public_dns,aws_instance.c01.*.private_dns]
+output "public_ip_info" {
+  value = {for instance in aws_instance.ad-lab: instance.key => instance.value.public_ip}
 }
 
+output "public_dns_info" {
+  value = {for instance in aws_instance.ad-lab: instance.key => instance.value.public_dns}
+}
+
+
+
+*/
